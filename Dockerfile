523e43b956e7aad5ade7f85fa980b2c9b17b93b2
Manish Vasu <manishvasu@Manishs-MacBook-Air.local>
2024-02-11 19:42:12 +0530

    Adds multiple query support

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,8 @@ WORKDIR /app
 
 COPY ./requirements.txt /app
 RUN pip install --no-cache-dir --upgrade -r requirements.txt
-RUN pip install wsgi
 
 COPY ./src /app
 
-CMD ["wsgi", "main:app", "--host", "0.0.0.0", "--port", "80"]
+# CMD ["wsgi", "main:app", "--host", "0.0.0.0", "--port", "80"]
+CMD ["python", "text2vec-bert.py"]
--- src/text2vec-bert.py
+++ src/text2vec-bert.py
@@ -1,48 +1,95 @@
 import torch
-from transformers import BertTokenizer, BertModel
+from transformers import BertTokenizerFast, BertModel
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
+import os
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu')
+# for local development 
+# MODEL_PATH = './src/assets/bert'
+# TOKENIZER_PATH = './src/assets/tokenizer'
+
+#for container deployment
+MODEL_PATH = './assets/bert'
+TOKENIZER_PATH = './assets/tokenizer'
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/vectorize")
-def vectorize():
-    if not request.args:
-        return "Provided invalid arguments"
-    
-    query = request.args["query"]
-    
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    encoded_dict = tokenizer.encode_plus(
-                            query,
+def load_model(model_path):
+    if os.listdir(model_path):
+        model = BertModel.from_pretrained(model_path)
+    else:
+        model = BertModel.from_pretrained('bert-base-uncased',
+                                  output_hidden_states = True
+                                  )
+        model.save_pretrained(model_path)
+
+    model.eval()
+
+    return model.to(DEVICE)
+
+def load_tokenizer(tokenizer_path):
+    if os.listdir(tokenizer_path):
+        tokenizer = BertTokenizerFast.from_pretrained(tokenizer_path)
+    else:
+        tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
+        tokenizer.save_pretrained(tokenizer_path)
+
+    return tokenizer
+
+def tokenize(queries, tokenizer):
+    encoded_dict = tokenizer.batch_encode_plus(
+                            queries,
                             add_special_tokens = True,
                             padding = 'max_length',
                             return_attention_mask = True,
                             return_tensors = 'pt',
                         )
 
-    input_ids = []
-    attention_masks = []
+    input_ids = list()
+    attention_masks = list()
 
     input_ids.append(encoded_dict['input_ids'])
     attention_masks.append(encoded_dict['attention_mask'])
 
-    input_ids = torch.cat(input_ids, dim=0)
-    attention_masks = torch.cat(attention_masks, dim=0)
+    input_ids = torch.cat(input_ids, dim=0).to(DEVICE)
+    attention_masks = torch.cat(attention_masks, dim=0).to(DEVICE)
 
-    model = BertModel.from_pretrained('bert-base-uncased',
-                                  output_hidden_states = True
-                                  )
-    model.eval()
+    return input_ids, attention_masks
 
-    with torch.no_grad():
-        outputs = model(input_ids, attention_masks)
-        hidden_states = outputs[2]
+def vectorize(input_ids, attention_masks, model):
+    output = list()
 
-    token_vecs = hidden_states[-2][0]
+    for i in range(len(input_ids)):
+        with torch.no_grad():
+            outputs = model(input_ids[i].unsqueeze(0), attention_masks[i].unsqueeze(0))
+            hidden_states = outputs[2]
 
-    sentence_embedding = torch.mean(token_vecs, dim=0)
+        token_vecs = hidden_states[-2][0]
 
-    return sentence_embedding.tolist()
+        sentence_embedding = torch.mean(token_vecs, dim=0)
+        output.append(sentence_embedding.to('cpu').tolist())
+
+    return output
+
+@app.route("/")
+@cross_origin()
+def main():
+    if not request.args:
+        return "Provided invalid arguments"
+    queries = request.args.getlist('query')
+    
+    tokenizer = load_tokenizer(TOKENIZER_PATH)
+    
+    input_ids, attention_masks = tokenize(queries, tokenizer)
+
+    model = load_model(MODEL_PATH)
+
+    output = vectorize(input_ids, attention_masks, model)
+
+    return output
 
 if __name__ == "__main__":
     app.run(debug=True)
